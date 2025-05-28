c81c50ccf3b8af5534ed95f8c5fbc58e013109c3
Władysław Czyżewski <wladyslaw@czyzewski.me>
2025-05-29 01:32:09 +0200

    removed gh release, fix docs

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare module "icu-iana-from-windows" {
    * Converts a Windows timezone identifier to an IANA timezone identifier.
    *
    * @param windowsTimezone - The Windows timezone identifier (e.g., "Pacific Standard Time").
-   * @param territory - Optional territory code (e.g., "US") to refine the conversion. If not provided, the function will use "ZZ" (Unknown territory).
+   * @param territory - Optional territory code (e.g., "US") to refine the conversion.
    * @returns The corresponding IANA timezone identifier (e.g., "America/Los_Angeles").
    */
   export function convertWindowsToIana(
